77c6aba54dc99445da102a005f3f33e31019ab97
Cubiksolve1 <michelecrapuzii@gmail.com>
2021-10-05 16:20:09 +0200

    int

--- app/src/main/java/com/example/managerapp/MainActivity.java
+++ app/src/main/java/com/example/managerapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private int ciao;
+    private int ciao2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
