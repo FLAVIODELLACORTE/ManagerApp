f04e085b1f1fb5823aef5116de89e597ff99b1b3
FLAVIODELLACORTE <flavio.dellacorte1997@gmail.com>
2021-10-05 16:21:43 +0200

    String

--- app/src/main/java/com/example/managerapp/MainActivity.java
+++ app/src/main/java/com/example/managerapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private int ciao2;
+    private String all;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
